22e56eadc6c56eeee4a328e35a2a4a14d74aeb94
abtin shadfar <shadf@Abtins-MacBook-Air.local>
2026-04-22 13:43:15 -0700

    everything

--- co2_stats.py
+++ co2_stats.py
@@ -7,6 +7,22 @@ import sys
 sys.setrecursionlimit(10**6)
 
 
+@dataclass (frozen=True)
+class Row:
+    country: str
+    year: int
+    electricity_and_heat_co2_emissions: Union[float, None]
+    electricity_and_heat_co2_emissions_per_capita: Union[float, None]
+    energy_co2_emissions: Union[float, None]
+    energy_co2_emissions_per_capita: Union[float, None]
+    total_co2_emissions_excluding_lucf: Union[float, None]
+    total_co2_emissions_excluding_lucf_per_capita: Union[float, None]
+
+RowList = Union[None, 'RLNode']
+@dataclass (frozen=True)
+class RLNode:
+    first: Row
+    rest: RLNode
 
 
 class Tests(unittest.TestCase):
