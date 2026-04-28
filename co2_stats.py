cc5df084a89b6396b07587576da8b563e570fd7c
qp1d0 <gurparsadkk@gmail.com>
2026-04-27 21:59:35 -0700

    answers blah

--- co2_stats.py
+++ co2_stats.py
@@ -68,30 +68,17 @@ def listlen(ll: RowList) -> int:
         return 0
     return 1 + listlen(ll.rest)
 
-# 'country' filed should only be comparable using 'equal' compare_type.
-# Numerical measurement fields of CO2 emissions only compared using 'less_than' 
-# - and 'greater_than' compare_type
-
-# 3 ways: Return all of the rows where a given field is present:
-# - 1) and is less than a specified value.
-# - 2) and is equal to a specified value.
-# - 3) and is greater than a specified value.
-
-'''field_name should be chosen from strs in first line of CSV file -> read from csv file funtion(?)'''
-# make clear in purpose statement about diff types for compare_val
-# maybe dont use helper functions??
-# single filter function with ..yeah
-
-
-# Filter's through csv file data and returns all the rows where a given field is 
-# present and either less than a specified value, equal to a specified value,
-# or greater than a specified value. 
 
 field_names = Literal['country', 'year', 'electricity_and_heat_co2_emissions', 
                       'electricity_and_heat_co2_emissions_per_capita',
                       'energy_co2_emissions', 'energy_co2_emissions_per_capita',
                       'total_co2_emissions_excluding_lucf',
                       'total_co2_emissions_excluding_lucf_per_capita']
+
+"""Filter's through csv file data and returns all the rows where a given field is 
+    present and either less than a specified value, equal to a specified value,
+    or greater than a specified value
+ Raises ValueError if a field uses a non-comparable type. """
 
 def filter(ll_rows: RowList, field_name: field_names, 
            compare_type: Literal['less_than', 'equal', 'greater_than'], 
@@ -135,34 +122,47 @@ def filter(ll_rows: RowList, field_name: field_names,
 # Must use 'listlen' and 'filter' to produce an answer.
 
 # Number of countries listed in 'll_Rows'.
-def answer_1(ll_Rows: RowList):
-    pass
+def answer_1(ll_rows: RowList, year: int) -> int:
+    return listlen(filter(ll_rows, 'year', 'equal', year))
 
 # Years represented in 'll_Rows' for Mexico.
-def answer_2(ll_Rows_Mex: RowList) -> RowList:
-    pass
+def answer_2(ll_rows_mex: RowList) -> RowList:
+    return filter(ll_rows_mex, 'country', 'equal', 'Mexico')
 
 # Countries with higher per-capita total CO2 emissions (exclude lucf)
 # - than US in 1990.
-def answer_3(ll_Rows: RowList) -> RowList:
-    pass
+def answer_3(ll_rows: RowList) -> RowList:
+    us_1990 = filter(ll_rows, 'country', 'equal', 'USA')
+    us_1990 = filter(us_1990, 'year', 'equal', 1990)
+    return filter(ll_rows, 'total_co2_emissions_excluding_lucf_per_capita', 
+                  'greater_than', 
+                  us_1990.first.total_co2_emissions_excluding_lucf_per_capita)
 
 # Countries with higher per-capita total CO2 emissions (exclude lucf)
 # - than US in 2020.
-def answer_4(ll_Rows: RowList) -> RowList:
-    pass
+def answer_4(ll_rows: RowList) -> RowList:
+    us_2020 = filter(filter(ll_rows, 'country', 'equal', 'USA'), 'year', 'equal', 2020)
+    return filter(
+        ll_rows,
+        'total_co2_emissions_excluding_lucf_per_capita',
+        'greater_than',
+        us_2020.first.total_co2_emissions_excluding_lucf_per_capita
+    )
 
-# Approximate opulation of Luxembourg in 2014 in people.
-def answer_5(ll_Rows: RowList):
-    pass
+# Approximate population of Luxembourg in 2014.
+def answer_5(ll_rows: RowList):
+    lux = filter(ll_rows, 'country', 'equal', 'Luxembourg')
+    lux = filter(lux, 'year', 'equal', 2014)
+    row = lux.first
+    return (row.total_co2_emissions_excluding_lucf * 1000000) / row.total_co2_emissions_excluding_lucf_per_capita
 
 # Increase in total electricity-and-heat emissions in China from 
 # - 1990 to 2020 in multiplier terms.
-def answer_6(ll_Rows: RowList):
+def answer_6(ll_rows: RowList):
     pass
 
 # China's electricity-and-heat emissions in 2070.
-def answer_7(ll_Rows: RowList):
+def answer_7(ll_rows: RowList):
     pass
 
 class Tests(unittest.TestCase):
@@ -182,7 +182,6 @@ class Tests(unittest.TestCase):
 
 
 # TEST CASES
-# (illegal test for filter)
 class Tests(unittest.TestCase):
     def test_filter_ValueError(self):
         lines : list[Row] = read_csv_lines('sample-file.csv')
@@ -211,7 +210,7 @@ class Tests(unittest.TestCase):
                                 RLNode(Row('Lithuania',1994,7.27,1.9281462,14.44,3.8297703,14.82,3.930554), 
                                     None))))
         self.assertEqual(filter(lines, 'energy_co2_emissions', 'greater_than', 13.50), equal_ans)
-      
+    
 
     def test_read_csv_lines_returns_rlnode(self):
         result = read_csv_lines('sample-file.csv')
